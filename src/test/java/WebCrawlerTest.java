cfea0653e1213084911b6dd0e260e9207e49b0ca
aoksy1 <amar.okic.ao@gmail.com>
2022-04-25 10:57:11 +0200

    v10
    Bug fixes
    
    Co-Authored-By: erminmiro <102598749+erminmiro@users.noreply.github.com>

--- src/test/java/WebCrawlerTest.java
+++ src/test/java/WebCrawlerTest.java
@@ -1,5 +1,3 @@
-import org.junit.Before;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import java.io.IOException;
 import static org.junit.jupiter.api.Assertions.assertEquals;
